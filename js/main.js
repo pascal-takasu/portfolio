a3c1cc4c4d5d8eca9a02768fb141f5575244a9ee
pascal-takasu <takasu@pascal.jp>
2024-08-07 16:53:30 +0900

    F4-②基礎構文 終了時点の状態保存

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-console.log("コンソール出力");
-//console.log("コンソール出力2");
-console.log("コンソール出力3");
+// console.log("コンソール出力");
+// console.log("コンソール出力2");
+// console.log("コンソール出力3");
+
+//変数の宣言
+let num = 30;
+//変数の出力
+console.log(num);
+
+//変数の上書き
+num = 50;
+//変数の再出力
+console.log(num);
+
+//定数の宣言
+const MESSAGE = "定数メッセージ";
+//定数の出力
+console.log(MESSAGE);
+
+//定数の上書き
+//MESSAGE = "定数メッセージの上書き";
+//定数の出力
+console.log(MESSAGE);
+
+//データ型の確認
+console.log(typeof 10);
+console.log(typeof "こんにちは");
+console.log(typeof true);
+
+//関数の定義
+function greet(name) {
+    console.log(name + "さん、こんにちは");
+}
+
+// 関数の呼び出し
+greet("山田");
+greet("鈴木");
