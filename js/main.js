cff98d29bf17c8b7034e57d361598d83b6b62a36
pascal-takasu <takasu@pascal.jp>
2024-08-07 18:57:56 +0900

    3 . ハンバーガーメニュー の実装

--- js/main.js
+++ js/main.js
@@ -1,98 +1,115 @@
-// console.log("コンソール出力");
-// console.log("コンソール出力2");
-// console.log("コンソール出力3");
+// // console.log("コンソール出力");
+// // console.log("コンソール出力2");
+// // console.log("コンソール出力3");
 
-//変数の宣言
-let num = 30;
-//変数の出力
-console.log(num);
+// //変数の宣言
+// let num = 30;
+// //変数の出力
+// console.log(num);
 
-//変数の上書き
-num = 50;
-//変数の再出力
-console.log(num);
+// //変数の上書き
+// num = 50;
+// //変数の再出力
+// console.log(num);
 
-//定数の宣言
-const MESSAGE = "定数メッセージ";
-//定数の出力
-console.log(MESSAGE);
+// //定数の宣言
+// const MESSAGE = "定数メッセージ";
+// //定数の出力
+// console.log(MESSAGE);
 
-//定数の上書き
-//MESSAGE = "定数メッセージの上書き";
-//定数の出力
-console.log(MESSAGE);
+// //定数の上書き
+// //MESSAGE = "定数メッセージの上書き";
+// //定数の出力
+// console.log(MESSAGE);
 
-//データ型の確認
-console.log(typeof 10);
-console.log(typeof "こんにちは");
-console.log(typeof true);
+// //データ型の確認
+// console.log(typeof 10);
+// console.log(typeof "こんにちは");
+// console.log(typeof true);
 
-//関数の定義
-function greet(name) {
-    console.log(name + "さん、こんにちは");
-}
+// //関数の定義
+// function greet(name) {
+//     console.log(name + "さん、こんにちは");
+// }
 
-// 関数の呼び出し
-greet("山田");
-greet("鈴木");
+// // 関数の呼び出し
+// greet("山田");
+// greet("鈴木");
 
-console.log(document.getElementById("menu"));
-//document.getElementById("menu").textContent = "MENUの書き換え";
+// console.log(document.getElementById("menu"));
+// //document.getElementById("menu").textContent = "MENUの書き換え";
 
-//クリックイベントで呼ばれる関数の定義
-function menuClick() {
-    document.getElementById("menu").textContent = "クリックされました";
-}
+// //クリックイベントで呼ばれる関数の定義
+// function menuClick() {
+//     document.getElementById("menu").textContent = "クリックされました";
+// }
 
-//クリックイベント
-document.getElementById("menu").onclick = menuClick;
+// //クリックイベント
+// document.getElementById("menu").onclick = menuClick;
 
-//マウスオーバーイベントで呼ばれる関数の定義
-function menuMouseOver() {
-    document.getElementById("menu").textContent = "マウスオーバーされました";
-}
+// //マウスオーバーイベントで呼ばれる関数の定義
+// function menuMouseOver() {
+//     document.getElementById("menu").textContent = "マウスオーバーされました";
+// }
 
-//マウスオーバーイベント
-document.getElementById("menu").onmouseover = menuMouseOver;
+// //マウスオーバーイベント
+// document.getElementById("menu").onmouseover = menuMouseOver;
 
-//スタイルの操作を行う関数の定義
-function headerMouseOver() {
-    document.getElementById("header").style.color = "red";
-}
+// //スタイルの操作を行う関数の定義
+// function headerMouseOver() {
+//     document.getElementById("header").style.color = "red";
+// }
 
-//スタイルの操作の呼び出し
-document.getElementById("header").onmouseover = headerMouseOver;
+// //スタイルの操作の呼び出し
+// document.getElementById("header").onmouseover = headerMouseOver;
 
-//classの追加・削除を行う関数の定義
-function headerOnClick() {
-    document.getElementById("header").classList.toggle("clicked");
-}
+// //classの追加・削除を行う関数の定義
+// function headerOnClick() {
+//     document.getElementById("header").classList.toggle("clicked");
+// }
 
-//classの追加・削除を行う関数の呼び出し
-document.getElementById("header").onclick = headerOnClick;
+// //classの追加・削除を行う関数の呼び出し
+// document.getElementById("header").onclick = headerOnClick;
 
-//モーダルを表示する関数の定義
-function modalOpen() {
-    console.log("modalOpen");
-    document.getElementById("modal").style.display = "block";
+// //モーダルを表示する関数の定義
+// function modalOpen() {
+//     console.log("modalOpen");
+//     document.getElementById("modal").style.display = "block";
+// };
+
+// //送信ボタンをクリックしたときにmodalOpen関数を呼び出す
+// document.getElementById("submit").onclick = modalOpen;
+
+// //モーダルを非表示にする関数の定義
+// function modalClose() {
+//     console.log("modalClose");
+//     document.getElementById("modal").style.display = "none";
+// };
+
+// //モーダル上で戻るボタンをクリックしたときにmodalClose関数を呼び出す
+// document.getElementById("modal-close").onclick = modalClose;
+
+// //ページを遷移させる関数の定義
+// function modalSubmit() {
+//     location = "complete.html";
+// };
+
+// //モーダル上で送信ボタンをクリックした時にmodalSubmit関数を呼び出す
+// document.getElementById("modal-submit").onclick = modalSubmit;
+
+//メニューを表示する関数の定義
+function menuOpen() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("humb-close").style.display = "block";
 };
 
-//送信ボタンをクリックしたときにmodalOpen関数を呼び出す
-document.getElementById("submit").onclick = modalOpen;
+//開くアイコンをクリックしたときにmenuOpen関数を呼び出す
+document.getElementById("humb-open").onclick = menuOpen;
 
-//モーダルを非表示にする関数の定義
-function modalClose() {
-    console.log("modalClose");
-    document.getElementById("modal").style.display = "none";
+//メニューを非表示にする関数の定義
+function menuClose() {
+    document.getElementById("menu").style.display = "none";
 };
 
-//モーダル上で戻るボタンをクリックしたときにmodalClose関数を呼び出す
-document.getElementById("modal-close").onclick = modalClose;
-
-//ページを遷移させる関数の定義
-function modalSubmit() {
-    location = "complete.html";
-};
-
-//モーダル上で送信ボタンをクリックした時にmodalSubmit関数を呼び出す
-document.getElementById("modal-submit").onclick = modalSubmit;
+//閉じるアイコンをクリックしたときにmenuClose関数を呼び出す
+document.getElementById("humb-close").onclick = menuClose;
